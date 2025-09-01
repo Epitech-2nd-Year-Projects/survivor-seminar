ab4b21674d4ca11c4d5c0d01f964baddcc867beb
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-09-01 12:55:06 +0000

    ci(web): fix typecheck script

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
@@ -15,7 +15,11 @@ const geist = Geist({
   variable: "--font-geist-sans",
 });
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <html lang="en" suppressHydrationWarning className={geist.className}>
@@ -32,6 +36,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </body>
       </html>
     </>
-  )
+  );
 }
-
