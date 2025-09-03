d5d21175c9c14194a44c8a8c552afc99b234e791
Gallini <enzo.gallini@epitech.eu>
2025-09-03 15:33:57 +0200

    feat(founders) : Add founders endpoint to server

---
+++ backend/internal/server/routes/v1/founders.go
@@ -0,0 +1,22 @@
+package v1
+
+import (
+	v1handlers "github.com/Epitech-2nd-Year-Projects/survivor-seminar/internal/handlers/v1"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+func RegisterFounders(r *gin.RouterGroup, db *gorm.DB, logger *logrus.Logger) {
+	h := v1handlers.NewFoundersHandler(db, logger)
+
+	r.GET("/founders", h.GetFounders)
+	r.GET("/founders/:id", h.GetFounder)
+	r.GET("/startups/:startup_id/founders", h.GetFoundersByStartup)
+	r.GET("/founders/:id/image", h.GetFounderImage)
+
+	admin := r.Group("/admin/founders")
+	admin.POST("", h.CreateFounder)
+	admin.PATCH("/:id", h.UpdateFounder)
+	admin.DELETE("/:id", h.DeleteFounder)
+}
