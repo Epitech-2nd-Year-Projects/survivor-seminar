b6d8539187ad4fd043e7d231fa8125ca5e847ed1
Gallini <enzo.gallini@epitech.eu>
2025-09-04 09:26:49 +0200

    feat(news) : Add news endpoints to server

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -101,7 +101,7 @@ func (s *HTTPServer) registerRoutes() {
 	v1routes.RegisterStartups(v1, s.db, s.log)
 	v1routes.RegisterFounders(v1, s.db, s.log)
 	v1routes.RegisterUsers(v1, s.db, s.log)
-	v1.Group("/news")
+	v1routes.RegisterNews(v1, s.db, s.log)
 	v1.Group("/events")
 	v1.Group("/sectors")
 	v1.Group("/locations")
