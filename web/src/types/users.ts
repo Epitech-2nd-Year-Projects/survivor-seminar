e8584e116027b3c71f38c6c1515a9043a6694be8
LaurentAliu7 <laurent.aliu@epitech.eu>
2025-09-05 17:34:36 +0200

    feat(web/type): add User type

---
+++ web/src/types/users.ts
@@ -0,0 +1,12 @@
+export interface User {
+  founder_id: number;
+  created_at: string;
+  email: string;
+  email_verified: boolean;
+  id: number;
+  image_url: string;
+  investor_id: number;
+  name: string;
+  role: string;
+  updated_at: string;
+}
